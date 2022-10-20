8c852900488ea177a35625dc7744056e3f03872a
mesutrk95 <mesut.rk95@gmail.com>
2022-10-21 02:41:06 +0330

    fix for 180deg turn and more logs.

--- robot-node-server/index.js
+++ robot-node-server/index.js
@@ -1,15 +1,25 @@
+
+const express = require('express');
+const cors = require('cors');
 const Gpio = require('pigpio').Gpio;
 const Camera = require('./Camera') 
 const Motors = require('./Motors') 
-
 const motors = new Motors(20, 16, 19, 26) 
- 
-const server = require('http').createServer();
-const io = require('socket.io')(server, {
-    cors: {
-      origin: '*',
-    }
-});
+
+var app = express();
+app.use(cors());
+
+var server = app.listen(5123, ()=>{
+  console.log('listening on ' + 5123)
+}); 
+
+  
+const io = require('socket.io')(server, { 
+  cors : { 
+    origin : '*'  
+  }
+}); 
+
   
 let cameraRequesterClient = null; 
  
@@ -39,39 +49,49 @@ io.on('connection', client => {
     let last = 0;
 
     client.on('action', async data => { 
-      let now = Date.now();
       const axis = { x: parseFloat(data.x), y: parseFloat(data.y)}
-      console.log('action', now - last, axis); 
-      last = now;  
   
-      if(axis.x == 0 && axis.y == 0){
-        motors.left.stop()
-        motors.right.stop()
-        return;
-      }
       let deg = axis2deg(axis)
       let power = Math.sqrt(axis.x * axis.x + axis.y * axis.y)
       let df = deg < 180 ? 'f' : 'b'; 
       let db = deg < 180 ? 'b' : 'f';
 
+      let now = Date.now();
+      console.log('action', now - last, 
+        'x:'+ axis.x.toFixed(4), 'y:'+ axis.y.toFixed(4), 
+        "p:" + power, 'deg:' + deg); 
+      last = now;  
+
+      if(Math.abs(axis.x) < 0.01 && Math.abs(axis.y) < 0.01){
+        motors.left.stop()
+        motors.right.stop()
+        return;
+      }
+
+      if(power > 1) power = 1
       if(deg > 180) deg = deg - 180 
        
-      if(deg < 45){
-        motors.right.turn(db, power * (1 - deg / 45))
-        motors.left.turn(df, power)
+
+      try{ 
+        if(deg < 45){
+          motors.right.turn(db, power * (1 - deg / 45))
+          motors.left.turn(df, power)
+        }
+        else if(deg < 90){
+          motors.right.turn(df, power * (-1 + deg / 45))
+          motors.left.turn(df, power)
+        }
+        else if(deg < 135){
+          motors.right.turn(df, power)
+          motors.left.turn(df, power * (3 - deg / 45))
+        }
+        else if(deg <= 180){
+          motors.right.turn(df, power)
+          motors.left.turn(db, power * (-3 + deg / 45))
+        } 
+      }catch(ex){
+        console.error(ex, deg + ' deg', power)
       }
-      else if(deg < 90){
-        motors.right.turn(df, power * (-1 + deg / 45))
-        motors.left.turn(df, power)
-      }
-      else if(deg < 135){
-        motors.right.turn(df, power)
-        motors.left.turn(df, power * (3 - deg / 45))
-      }
-      else if(deg < 180){
-        motors.right.turn(df, power)
-        motors.left.turn(db, power * (-3 + deg / 45))
-      } 
  
       if(timeoutHandle){ 
         clearTimeout(timeoutHandle)
@@ -108,11 +128,7 @@ io.on('connection', client => {
       }
 
     });
-});
-server.listen(5123, ()=>{
-  console.log('listening on ' + 5123)
-});
-
+}); 
 process.on('SIGINT', async function () {
   motors.reset();  
   await camera.stop();
